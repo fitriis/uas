8a22008c6d43cd23ed13828962c4a6a80870e5ba
fitriis <indahsarifitri916@gmail.com>
2022-01-04 20:32:22 +0700

    commit

---
+++ resources/views/profil_unisbank.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>profil unisbank</title>
+
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/unicons.css">
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+  <link rel="stylesheet" href="css/owl.theme.default.min.css">
+
+  <!-- MAIN STYLE -->
+  <link rel="stylesheet" href="css/tooplate-style.css">
+
+  <style>
+    body {
+        background-color: #1c202c;
+        color:red;
+    }
+
+  </style>
+
+</head>
+
+<body>
+
+
+<h2>S1 TEKNIK INFORMATIKA</h2>
+<p>Program studi Teknik Informatika <br>
+Belajar dan menerapkan prinsip-prinsip ilmu komputer dan <br>
+analisa matematis untuk desain, pengembangan, pengujian dan <br>
+evaluasi perangkat lunak, sistem operasi dan kerja komputer.
+</p>
+
+</body>
+
+</html>
---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/profil_saya', function () {
+    return view('profil_saya');
+});
+
+Route::get('/profil_unisbank', function () {
+    return view('profil_unisbank');
+});
+
